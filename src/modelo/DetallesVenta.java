035a8bcaf51473098f6dc6c6a5c4cb2e60766a6d
Camilo345 <jcamiloB345@users.noreply.github.com>
2020-05-10 16:06:37 -0500

    Agregando metodo buscarDetallesVenta al modelo DetallesVenta

--- src/modelo/DetallesVenta.java
+++ src/modelo/DetallesVenta.java
@@ -5,7 +5,12 @@
  */
 package modelo;
 
+import control.BaseDatos;
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
 
 /**
  *
@@ -87,6 +92,39 @@ public class DetallesVenta {
         return "DetallesVenta{" + "IDdetalleVenta=" + IDdetalleVenta + ", fechaPago=" + fechaPago + ", fechaEntrega=" + fechaEntrega + ", cantidadProductos=" + cantidadProductos + ", IDProductof3=" + IDProductof3 + ", IDinventariof=" + IDinventariof + '}';
     }
     
-    
+    public LinkedList<DetallesVenta> buscarDetallesVenta(String sql) {
+       
+        ResultSet rs = null;
+        LinkedList<DetallesVenta> ldv = new LinkedList<>();
+        BaseDatos objcone = new BaseDatos();
+     int IDdetalleVenta2=0;
+    Date fechaPago2;
+    Date fechaEntrega2;
+    String cantidadProductos2="";
+    int IDProductof32=0;
+    int IDinventariof2=0;
+        if (objcone.crearConexion()) {
+            try {
+                Statement sentencia = objcone.getConexion().createStatement();
+                rs = sentencia.executeQuery(sql);
+                while (rs.next()) {
+                    IDdetalleVenta2 = rs.getInt("IDdetalleVenta");
+                    fechaPago2 = rs.getDate("fechaPago");
+                    fechaEntrega2 = rs.getDate("fechaEntrega");
+                    cantidadProductos2 = rs.getString("cantidadProductos");
+                    IDProductof32 = rs.getInt("IDProductof3");
+                    IDinventariof2 = rs.getInt("IDinventariof");
+                   
+                 
+                   ldv.add(new DetallesVenta(IDdetalleVenta2, fechaPago2, fechaEntrega2, cantidadProductos2, IDProductof32,IDinventariof2));
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+
+            }
+        }
+        return ldv;
+        
+    }
     
 }
